eff0b4d053ed989dddabd6da25888b3762c46ecb
Charles <cming.chou@gmail.com>
2017-04-12 20:18:15 +0000

    added link delete functionality

--- express_server.js
+++ express_server.js
@@ -12,7 +12,7 @@ const urlDatabase = {
 };
 
 function generateRandomString(){
-  const x = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
+  const x = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
   let rnd = '';
   for( let i = 0; i < 6; i++){
     rnd += x.charAt(Math.floor(Math.random() * 62));
@@ -21,34 +21,41 @@ function generateRandomString(){
 }
 
 
-
-app.get("/urls/new", (req, res) => {
+app.get('/urls/new', (req, res) => {
   res.render("urls_new");
 });
 
-app.post("/urls", (req, res) => {
+app.post('/urls', (req, res) => {
   let randStr = generateRandomString();
   urlDatabase[randStr] = req.body.longURL;
-  res.redirect(303, '/urls/'+randStr);
+  res.redirect(303, '/urls/' + randStr);
   console.log(req.body, urlDatabase);
   // res.send("ok");
 });
 
-app.get("/urls", (req, res) => {
-  let templateVars = {urls: urlDatabase};
-  res.render("urls_index", templateVars);
+app.post('/urls/:id/delete', (req, res) => {
+  delete urlDatabase[req.params.id];
+  res.redirect('/urls');
 });
+
+app.get('/urls', (req, res) => {
+  let templateVars = {urls: urlDatabase};
+  res.render('urls_index', templateVars);
+});
+
+
+
 //
-app.get("/urls/:id", (req,res) => {
+app.get('/urls/:id', (req, res) => {
   let templateVars = {
     shortURL: req.params.id,
     fullURL: urlDatabase[req.params.id]
   };
-  res.render("urls_show", templateVars);
+  res.render('urls_show', templateVars);
 });
 
-app.get("/u/:shortURL", (req, res) => {
-  let longURL = urlDatabase[req.params.shortURL];
+app.get('/u/:shortURL', (req, res) => {
+  let longURL = `${urlDatabase[req.params.shortURL]}`;
   // let longURL =  ...
   res.redirect(longURL);
 });
