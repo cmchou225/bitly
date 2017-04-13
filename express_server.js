ad6182ee59b49298f756497a7ef2479f30692568
Charles <cming.chou@gmail.com>
2017-04-13 20:52:24 +0000

    added registration page with input fields verification

--- express_server.js
+++ express_server.js
@@ -4,19 +4,40 @@ const app = express();
 const PORT = process.env.PORT || 8080;
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
+const bcrypt = require('bcrypt');
 
+//Engines
 app.set('view engine', 'ejs');
 
 // Middleware
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({extended: true}));
+app.use((req, res, next) => {
+  res.locals.username = req.body.username;
+  next();
+});
+//-- App local variables
 
 //---Database
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
-// --- random string generator
+
+const users = [
+  {
+    id: "userRandomID",
+    email: "user@example.com",
+    password: "purple-monkey-dinosaur"
+  },
+  {
+    id: "user2RandomID",
+    email: "user2@example.com",
+    password: "dishwasher-funk"
+  }
+];
+
+// Helper Func --- random string generator
 function generateRandomString(){
   const x = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
   let rnd = '';
@@ -26,13 +47,50 @@ function generateRandomString(){
   return rnd;
 }
 
+
+// Root page
+app.get('/', (req, res) => {
+  res.send('helloooooo');
+});
+
+//Registeration path
+app.get('/register', (req, res) => {
+  res.render('urls_reg');
+});
+
+app.post('/register', (req, res) => {
+  const userID = generateRandomString();
+  const newEmail = req.body.email;
+  const inDB = users.find((obj) => obj.email === newEmail);
+  //const userByEmail = users.find((userObj) => req.body.email === userObj.email);
+  if(!newEmail || !req.body.password){
+    res.status(400);
+    res.send('Please input both email and password');
+  }
+  else if(inDB){
+    res.status(400);
+    res.send('Email in use');
+  }
+  else {
+    let newUser = {
+      id: userID,
+      email: req.body.email
+      };
+    bcrypt.hash(req.body.password, 10, (err, hash) => {
+      newUser.password = hash;
+      users.push(newUser);
+    });
+    res.cookie('userid', userID);
+    res.redirect('/');
+  }
+});
+
 //--GET request to home page
 app.get('/urls', (req, res) => {
   let templateVars = {
     username: req.cookies['username'],
     urls: urlDatabase,
   };
-  console.log(templateVars.username);
   res.render('urls_index', templateVars);
 });
 
@@ -43,7 +101,8 @@ app.post('/login', (req, res) => {
 })
 // GET request for new path
 app.get('/urls/new', (req, res) => {
-  res.render("urls_new");
+  let templateVars = { username: req.cookies['username']};
+  res.render("urls_new", templateVars);
 });
 
 //POST request logout
